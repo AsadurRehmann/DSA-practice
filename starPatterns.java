637b6042d1795946c36f34ac0e220841add1e124
AsadurRehmann <asadurrehman886@gmail.com>
2025-07-21 19:45:25 +0500

    first

---
+++ starPatterns.java
@@ -0,0 +1,13 @@
+ public class starPatterns {
+    public static void main(){
+        
+        for(int i=0; i<=4; i++){
+            for(int j=0; j<=i; j++){
+            System.out.print("*");
+            
+        }
+        System.out.println();
+    }
+
+    }
+}
