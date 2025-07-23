5d189f762de61ca65e1110b1d0b8edaa60c3d4c5
AsadurRehmann <asadurrehman886@gmail.com>
2025-07-23 06:15:44 +0500

    Hell yeahh

---
+++ decimalToBinary.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class decimalToBinary {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter a integer:");
+        int input = sc.nextInt();
+        String result="";
+        int x=0;
+
+        while (input>0) {
+            if (input%2==0) {
+            while (input%2==0) {
+                 x=input/2;
+                 input/=2;
+                 result+="0";
+            }
+        }
+
+        if ((input%2==1)) {
+            while (input%2==1) {
+                 x=input/2;
+                 input/=2;
+                 result+="1";
+            }
+        } 
+        }
+       
+        result=new StringBuilder(result).reverse().toString();
+        System.out.println("Binary:"+result);
+        sc.close();
+    }
+}
