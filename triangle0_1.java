3adaeb01e4ce9214f92adaa6a5b3e96b9c92131f
AsadurRehmann <asadurrehman886@gmail.com>
2025-07-24 09:58:02 +0500

    floyd and 01 triangle

---
+++ triangle0_1.java
@@ -0,0 +1,15 @@
+public class triangle0_1 {
+    public static void main(String[] args) {
+        for(int i=0; i<=4; i++){
+            for(int j=0; j<=i; j++){
+                if((i+j)%2==0){
+                    System.out.print("0");
+                }
+                if((i+j)%2!=0){
+                    System.out.print("1");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
