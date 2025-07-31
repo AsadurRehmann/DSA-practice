d960f4bc176906547cdf9722591fc1aec1a78026
AsadurRehmann <asadurrehman886@gmail.com>
2025-07-31 06:24:27 +0500

    tweaking with arrays

--- Problems/reverseArray.java
+++ Problems/reverseArray.java
@@ -16,7 +16,6 @@ public class reverseArray {
     }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6,7,8,9,10,11};
-
         System.out.println("reversed array: " + Arrays.toString(reverse(arr)));
     }
 }
